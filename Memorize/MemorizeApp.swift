bf1afd6ede7cf7a24a2a04784d4f391868f5255e
bbaneydev <baneyjb@gmail.com>
2024-07-15 14:33:21 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Billy Baney on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
